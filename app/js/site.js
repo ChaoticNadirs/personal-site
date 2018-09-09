c9d8ea64dfce22ab01f29fd8a3f6ea1a2eef7f1b
Christian Coda <christiancoda@outlook.com>
2018-09-09 16:55:49 +0100

    personal site v2

--- app/js/site.js
+++ app/js/site.js
@@ -1,93 +1,69 @@
-(function ($) {
+$("#copyright-year").text(new Date().getFullYear());
 
-    var wow = new WOW({
-        mobile: false
-    });
+$(window).on("load", function() {
+  $("#loading-status").fadeOut();
+  $("#loading")
+    .delay(500)
+    .fadeOut("slow");
+});
 
-    wow.init();
+$("a[href*=\\#]").click(function(e) {
+  var that = $(this);
+  $("html, body").animate(
+    {
+      scrollTop: $(that.attr("href")).offset().top
+    },
+    800
+  );
+  e.preventDefault();
+});
 
-    $('#copyright-year').text((new Date).getFullYear());
+$(".navbar-nav>li>a").on("click", function() {
+  $(".navbar-collapse").collapse("hide");
+});
 
-    $(window).on('load', function () {
-        $('#loading-status').fadeOut();
-        $('#loading').delay(350).fadeOut('slow');
-    });
+$(window).scroll(function() {
+  function elementScrolled(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    return elemTop <= docViewBottom && elemTop >= docViewTop;
+  }
 
-    $('a[href*=\\#]').click(function (e) {
-        var that = $(this);
-        $('html, body').animate({
-            scrollTop: $(that.attr('href')).offset().top
-        }, 800);
-        e.preventDefault();
-    });
+  if (elementScrolled("#progress-bar-backend")) {
+    $("#progress-bar-backend").css("width", "90%");
+  }
 
-    var color1 = '#EDB92E';
-    var color2 = '#F85931';
-    var color3 = '#CE1836';
-    var color4 = '#009989';
+  if (elementScrolled("#progress-bar-frameworks")) {
+    $("#progress-bar-frameworks").css("width", "60%");
+  }
 
-    var bar1 = new ProgressBar.Circle('#chart-1', {
-        strokeWidth: 6,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: color1,
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: null,
-        text: {
-            value: '9 Years'
-        }
-    });
+  if (elementScrolled("#progress-bar-db")) {
+    $("#progress-bar-db").css("width", "60%");
+  }
 
-    var bar2 = new ProgressBar.Circle('#chart-2', {
-        strokeWidth: 6,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: color2,
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: null,
-        text: {
-            value: '8 Years'
-        }
-    });
+  if (elementScrolled("#progress-bar-frontend")) {
+    $("#progress-bar-frontend").css("width", "80%");
+  }
 
-    var bar3 = new ProgressBar.Circle('#chart-3', {
-        strokeWidth: 6,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: color3,
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: null,
-        text: {
-            value: '6 Years'
-        }
-    });
+  if (elementScrolled("#progress-bar-aws")) {
+    $("#progress-bar-aws").css("width", "50%");
+  }
 
-    var bar4 = new ProgressBar.Circle('#chart-4', {
-        strokeWidth: 6,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: color4,
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: null,
-        text: {
-            value: '7 Years'
-        }
-    });
+  if ($(this).scrollTop() > 20) {
+    $(".navbar")
+      .removeClass("navbar-dark bg-transparent navbar-at-top")
+      .addClass("navbar-light bg-light navbar-scrolled");
+  } else {
+    $(".navbar")
+      .removeClass("navbar-light bg-light navbar-scrolled")
+      .addClass("navbar-dark bg-transparent navbar-at-top");
+  }
+});
 
-    var animated = false;
-
-    $(window).scroll(function () {
-        if (!animated && $('#chart-1').offset().top < $(this).height() + $(this).scrollTop()) {
-            bar1.animate(0.9);
-            bar2.animate(0.8);
-            bar3.animate(0.6);
-            bar4.animate(0.7);
-            animated = true;
-        }
-    });
-
-})(jQuery);
+var typed = new Typed("#intro-description", {
+  strings: ["Software Developer", "Team Leader"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
