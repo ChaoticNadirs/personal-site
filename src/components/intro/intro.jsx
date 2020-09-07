6bd41f1278623d5b874096aab52189d319496b5f
Christian Coda <christiancoda@gmail.com>
2020-09-07 09:34:49 +0100

    background image without gatsby

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,46 +1,113 @@
 import React from "react";
 import styled from "styled-components";
-import BackgroundImage from "gatsby-background-image";
-import { useStaticQuery, graphql } from "gatsby";
+// import BackgroundImage from "gatsby-background-image";
+// import { useStaticQuery, graphql } from "gatsby";
+import Avatar from "../avatar/avatar";
+import ScrollIcon from "../scroll-icon/scroll-icon";
+import bgMobile from "../../images/waterfall.jpg";
+import bgDesktop from "../../images/lake.jpg";
 
-const Header = styled(BackgroundImage)`
+const Header = styled.header`
+  background-image: url(${bgMobile});
   background-repeat: no-repeat;
   background-position: 100% 50%;
+  background-size: cover;
   background-attachment: fixed;
   height: 100%;
+  position: relative;
+  text-align: center;
+
+  @media screen and (orientation: landscape) {
+    background-image: url(${bgDesktop});
+  }
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Content = styled.div`
+  color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  width: 90%;
+`;
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  text-transform: uppercase;
+  letter-spacing: 0.125rem;
+  font-weight: 700;
+
+  ${(props) => props.theme.breakpoints.md} {
+    font-size: 2.5rem;
+  }
+`;
+
+const Subheading = styled.h2`
+  font-size: 1.125rem;
+  font-weight: 700;
+  letter-spacing: 0.125rem;
+  margin-bottom: 0;
+
+  ${(props) => props.theme.breakpoints.md} {
+    font-size: 1.5rem;
+  }
+`;
+
+const StyledScrollIcon = styled(ScrollIcon)`
+  margin-top: 5rem;
 `;
 
 const Intro = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      mobileImage: file(relativePath: { eq: "waterfall.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 768, quality: 100) {
-            ...GatsbyImageSharpFluid_withWebp
-            ...GatsbyImageSharpFluidLimitPresentationSize
-          }
-        }
-      }
-      desktopImage: file(relativePath: { eq: "lake.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1920, quality: 100) {
-            ...GatsbyImageSharpFluid_withWebp
-            ...GatsbyImageSharpFluidLimitPresentationSize
-          }
-        }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     mobileImage: file(relativePath: { eq: "waterfall.jpg" }) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 768, quality: 100) {
+  //           ...GatsbyImageSharpFluid_withWebp
+  //           ...GatsbyImageSharpFluidLimitPresentationSize
+  //         }
+  //       }
+  //     }
+  //     desktopImage: file(relativePath: { eq: "lake.jpg" }) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 1920, quality: 100) {
+  //           ...GatsbyImageSharpFluid_withWebp
+  //           ...GatsbyImageSharpFluidLimitPresentationSize
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
 
-  const sources = [
-    data.mobileImage.childImageSharp.fluid,
-    {
-      ...data.desktopImage.childImageSharp.fluid,
-      media: `(orientation: landscape)`,
-    },
-  ];
+  // const sources = [
+  //   data.mobileImage.childImageSharp.fluid,
+  //   {
+  //     ...data.desktopImage.childImageSharp.fluid,
+  //     media: `(orientation: landscape)`,
+  //   },
+  // ];
 
-  return <Header Tag="header" fluid={sources} backgroundColor="#040e18" />;
+  return (
+    // <Header Tag="header" fluid={sources} backgroundColor="#040e18">
+    <Header>
+      <Overlay />
+      <Content>
+        <Avatar />
+        <Heading>Christian Coda</Heading>
+        <Subheading>Software Engineer</Subheading>
+        <StyledScrollIcon />
+      </Content>
+    </Header>
+  );
 };
 
 export default Intro;
